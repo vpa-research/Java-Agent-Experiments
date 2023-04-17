ee3fda467a906bf2434a313dfe716598717ade56
alexeev509 <alexeevy509@gmail.com>
2023-04-17 23:09:35 +0300

    List field
    List<Integer> list2 = new LinkedList<>();

--- src/test/java/LinkedListFieldListInClassTest.java
+++ src/test/java/LinkedListFieldListInClassTest.java
@@ -2,19 +2,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class LinkedListFieldListInClassTest {
 
     LinkedList<Integer> list = new LinkedList<>();
+    List<Integer> list2 = new LinkedList<>();
 
     @Test
-    public void addTest() {
+    public void addLinkedListTest() {
         System.out.println("--START OF THE TEST LinkedListFieldListInClassTest--\n\n\n");
         list.add(1);
         list.add(2);
         Assert.assertEquals(2,list.size());
         Assert.assertEquals(Integer.valueOf(1),list.get(0));
         Assert.assertEquals(Integer.valueOf(2),list.get(1));
+        System.out.println("\n\n\n--END OF THE TEST LinkedListFieldListInClassTest--");
+    }
+
+    @Test
+    public void addListTest() {
+        System.out.println("--START OF THE TEST LinkedListFieldListInClassTest--\n\n\n");
+        list2.add(1);
+        list2.add(2);
+        Assert.assertEquals(2,list2.size());
+        Assert.assertEquals(Integer.valueOf(1),list2.get(0));
+        Assert.assertEquals(Integer.valueOf(2),list2.get(1));
         System.out.println("\n\n\n--END OF THE TEST LinkedListFieldListInClassTest--");
     }
 }
