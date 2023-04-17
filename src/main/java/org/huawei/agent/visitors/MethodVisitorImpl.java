e11c64254d629e7f0a395bcad8dca48222b5e23a
alexeev509 <alexeevy509@gmail.com>
2023-04-17 17:58:59 +0300

    New GenLinkedList
    Was created new generated class "GenLinkedList" successor of the LinkedList;
    Was added new test-case;

--- src/main/java/org/huawei/agent/visitors/MethodVisitorImpl.java
+++ src/main/java/org/huawei/agent/visitors/MethodVisitorImpl.java
@@ -13,7 +13,7 @@ public class MethodVisitorImpl extends MethodVisitor  {
     public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
         System.out.println("Name of the method: " + name+" Descriptor of the method: "+ descriptor+" OWNER: " + owner);
         if("java/util/LinkedList".equals(owner)){
-            owner = "org/huawei/GeneratedLinkedList";
+            owner = "org/huawei/GenLinkedList";
         }
         super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
     }
@@ -22,7 +22,7 @@ public class MethodVisitorImpl extends MethodVisitor  {
     public void visitTypeInsn(int opcode, String type) {
         System.out.println("TYPE: "+type);
         if("java/util/LinkedList".equals(type)){
-            type = "org/huawei/GeneratedLinkedList";
+            type = "org/huawei/GenLinkedList";
         }
         super.visitTypeInsn(opcode, type);
     }
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 public class LinkedListTest {
 
     @Test
-    public void linkedListAddTest() {
+    public void addMethodTest() {
         System.out.println("--START OF THE TEST--\n\n\n");
 
         LinkedList<Object> list = new LinkedList<>();
@@ -16,6 +16,29 @@ public class LinkedListTest {
 
         Assert.assertEquals(1, size);
         Assert.assertEquals(2, list.get(0));
+
+        System.out.println("\n\n\n--END OF THE TEST--");
+    }
+
+    @Test
+    public void removeMethodTest() {
+        System.out.println("--START OF THE TEST--\n\n\n");
+
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+
+
+        int size = list.size();
+        Assert.assertEquals(3, size);
+
+        Integer remove = list.remove(1);
+        Assert.assertEquals(Integer.valueOf(3), remove);
+
+        size = list.size();
+        Assert.assertEquals(2, size);
 
         System.out.println("\n\n\n--END OF THE TEST--");
     }
