ed2e8c714fc497fe4783aa724e5344e658b86194
alexeev509 <alexeevy509@gmail.com>
2023-05-09 20:49:55 +0300

    Little fixes in getCommonSuperClass method

--- src/main/java/org/huawei/agent/writers/ClassWriterImpl.java
+++ src/main/java/org/huawei/agent/writers/ClassWriterImpl.java
@@ -4,6 +4,8 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ClassWriterImpl extends ClassWriter {
 
@@ -19,18 +21,56 @@ public class ClassWriterImpl extends ClassWriter {
     //https://stackoverflow.com/questions/69563714/linkageerror-attempted-duplicate-class-definition-when-dynamically-instrument
     @Override
     protected String getCommonSuperClass(String type1, String type2) {
+
         try {
+            List<String> aSuperClasses = new LinkedList<>();
+            List<String> bSuperClasses = new LinkedList<>();
             ClassReader aClass = new ClassReader(type1);
             ClassReader bClass = new ClassReader(type2);
             String aSuper = aClass.getSuperName();
             String bSuper = bClass.getSuperName();
+//            System.out.println("aClass: " + aClass.getClassName()+" bClass: " + bClass.getClassName() +
+//                    " aSuper: " + aSuper + " bSuper:" + bSuper);
+            //1) aClass extends bClass
             if(aSuper.equals(bClass.getClassName()))
                 return aSuper;
+            //2) bClass extends aClass
             else if(bSuper.equals(aClass.getClassName()))
                 return bSuper;
+            //3)
+            else if(!isInterface(aClass) && !isInterface(bClass)){
+                do{
+                    String className = aClass.getClassName();
+                    if("java/lang/Object".equals(className))
+                        break;
+                    aSuperClasses.add(className);
+                    aClass = new ClassReader(aClass.getSuperName());
+                } while (true);
+
+                do{
+                    String className = bClass.getClassName();
+                    if("java/lang/Object".equals(className))
+                        break;
+                    bSuperClasses.add(className);
+                    bClass = new ClassReader(bClass.getSuperName());
+                } while (true);
+//                System.out.println(aSuperClasses);
+//                System.out.println(bSuperClasses);
+                for (String aSuperClass : aSuperClasses) {
+                    for (String bSuperClass : bSuperClasses) {
+                        if(aSuperClass.equals(bSuperClass))
+                            return aSuperClass;
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "java/lang/Object";
+    }
+
+    //https://stackoverflow.com/questions/40119773/is-there-a-way-to-tell-if-a-class-is-an-interface
+    public boolean isInterface(ClassReader cr) {
+        return ((cr.getAccess() & 0x200) != 0);
     }
 }
